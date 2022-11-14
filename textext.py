bcc9f915061a8c349297dc5105e2e7dddbb1f35c
ttoommxx <ttoommxx@gmail.com>
2022-11-14 17:38:28 +0000

    first commit, added void.txt, removed curly brackets from printing and move admissible list to separate file called admitted.txt

--- textext.py
+++ textext.py
@@ -1,42 +1,47 @@
 import os
-#import time if need to debug, time.sleep(seconds)
+# import time if need to debug, time.sleep(seconds)
 
-#create a list of existing exceptions
-exceptions = [e[:-3] for e in os.listdir("./Exceptions/subroutines/")]
+# create a list of existing exceptions
+exceptions = [e[:-3] for e in os.listdir('./Exceptions/subroutines/')]
+# fetch list of commands that should not produce any text output
+void_temp = open('./Exceptions/void.txt','r')
+void = [line[1:-1] for line in void_temp.readlines()]
+# list of admissible characters for commands
+admitted_temp = open('./Exceptions/admitted.txt','r')
+admitted = [line[:-1] for line in admitted_temp.readlines()]
 
-#this will be the final text
+# this will be the final text
 newText = ''
 
 dicSub = {'j':int, 'readText':str, 'writeText':str}
-#list of admissible characters for commands
-admissibleList = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','*']
 
-#read main file latex
+# read main file latex
 text = open('main.tex', 'r')
 oldText = text.read()
 i=0
 while i<len(oldText):
     if oldText[i] == '\\':
-        #when find backslash, start reading the command with j
+        # when find backslash, start reading the command with j
         j=i+1
-        #stop when the command has characters that are not admissible for commands
-        while oldText[j] in admissibleList:
+        # stop when the command has characters that are not admissible for commands
+        while oldText[j] in admitted:
             j=j+1
-        #create path
+        # create path
         command = "./Exceptions/subroutines/" + oldText[i+1:j] + ".py"
+        print(command)
         if os.path.exists(command):
             dicSub['j'] = j
             dicSub['readText'] = oldText[j:]
             dicSub['writeText'] = newText
             exec(open(command).read(),dicSub)
-            #after executing the command, update j and newText
+            # after executing the command, update j and newText
             j = dicSub['j']
             newText = dicSub['writeText']
-        else:
+        elif oldText[i+1:j] not in void:
             print('error 404: "' + oldText[i+1:j] + '" not found in ./Exceptions/subroutines/')
             break
         i = j
-    else:
+    elif oldText[i] != '{' and oldText[i] != '}':
         newText = newText + oldText[i]
     i=i+1
 
