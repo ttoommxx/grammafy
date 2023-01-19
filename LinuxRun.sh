a87355083979f7e7d54a1121e01c169b2a89f661
tommaso seneci surface <ttoommxx@gmail.com>
2023-01-19 14:51:36 +0000

    refined the linux script so that it opens the grammafied text right after

--- LinuxRun.sh
+++ LinuxRun.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-python grammafy.py
-read
+text=$(python grammafy.py)
+echo "$text" | sed '$d' # print everything but the last line
+echo "Do you want to open the output? Y/N"
+read answer
+if [ "${answer,}" = "y" ]; then 
+	xdg-open ${text##*$'\n'} # only takes the last variable
+fi
