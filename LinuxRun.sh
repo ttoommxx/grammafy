4b5b4dcf73a60bbe3267bd087041c2ef9d54382f
tommaso seneci surface <ttoommxx@gmail.com>
2023-01-30 14:52:31 +0000

    change name to python3 from python

--- LinuxRun.sh
+++ LinuxRun.sh
@@ -4,7 +4,7 @@ echo "Select the tex file"
 read
 ./modules/fff-2.2_read/fff
 
-text=$(python grammafy.py)
+text=$(python3 grammafy.py)
 echo "$text" # print everything but the last line
 echo "Press enter to open the output"
 read -n1 KEY
