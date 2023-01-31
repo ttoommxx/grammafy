78e0772102598f6cc2f94d26365d9579a376e99b
tommaso seneci surface <ttoommxx@gmail.com>
2023-01-31 13:26:41 +0000

    updated pyleManager

--- pyleManager.py
+++ pyleManager.py
@@ -2,6 +2,9 @@ import os, sys, termios, tty
 
 local_folder = os.path.abspath(os.getcwd()) + '/' # save original path
 index = 0 # dummy index
+hidden = False # toggle hidden
+dimension = False # toggle file_size
+instructions = 'INSTRUCTIONS:\n\n  leftArrow = previous folder\n  rightArrow = open folder\select file\n  upArrow = up\n  downArrow = down\n  q = quit\n  h = toggle hidden files\n  d = toggle file size\n  prefix ■ means folder\n\npress any button to continue'
 
 def main():
     pass
@@ -9,40 +12,67 @@ def main():
 if __name__ == "__main__":
     print('Press enter')
 
-def directory(): # print folders and files that are not hidden, in cronological order
-    dirs = sorted([x for x in os.listdir() if os.path.isdir(os.path.abspath(os.getcwd()) + '/' + x) and not x.startswith('.')], key=lambda s: s.lower())
-    files = sorted([x for x in os.listdir() if x not in dirs and not x.startswith('.')], key=lambda s: s.lower())
+# RETURN FILE SIZE AS A STRING
+def file_size(path):
+    size = os.stat(path).st_size
+    i = 0
+    while size > 999:
+        size = size / 1000
+        i = i+1
+    metric = ['b','kb','mb','gb']
+    return str(round(size,2)) + ' ' + metric[i]
+
+# LIST OF FOLDERS AND FILES
+def directory():
+    global hidden
+    dirs = sorted([x for x in os.listdir() if os.path.isdir(os.path.abspath(os.getcwd()) + '/' + x) and (hidden or not x.startswith('.') )], key=lambda s: s.lower())
+    files = sorted([x for x in os.listdir() if x not in dirs and (hidden or not x.startswith('.') )], key=lambda s: s.lower())
     return dirs + files
 
+# CLEAN TERMINAL
 def clear():
-    if os.name == 'nt': # clean terminal
+    if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
-def dir_printer(): # priting function
+# INDEX UPDATER
+def index_dir():
+    global index
+    if len(directory()) > 0:
+        index = index % len(directory())
+    else:
+        index = 0
+
+# PRINTING FUNCTION
+def dir_printer():
     global index
     clear()
-    # instructions on how to use it
-    print('INSTRUCTIONS: (leftArrow = previous folder), (rightArrow = open folder\select file), (upArrow = up), (downArrow = down), (q = quit), (* are folders)\n')
     # path directory
-    print(os.path.abspath(os.getcwd()) + '/\n')
+    print('press i for instructions\n\n' + os.path.abspath(os.getcwd()) + '/\n')
     # folders and pointer
     if len(directory()) == 0:
         print('**EMPTY FOLDER**')
     else:
+        index_dir()
         temp_sel = directory()[index]
+        l = max([len(x) for x in directory()]) + 4 # max length file
         for x in directory():
             if x == temp_sel:
                 print('->', end='')
             else:
                 print('  ', end='')
             if os.path.isdir(os.path.abspath(os.getcwd()) + '/' + x):
-                print('*', end='')
+                print('■', end='')
             else:
                 print(' ', end='')
-            print(x)
+            print(x, end=' ')
+            if dimension and os.path.isfile(os.path.abspath(os.getcwd()) + '/' + x):
+                print(' '*(l-len(x)) + file_size(os.path.abspath(os.getcwd()) + '/' + x))
+            else:
+                print()
 
+# FETCH KEYBOARD INPUT
 def getch():
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -55,51 +85,53 @@ def getch():
 
 dir_printer()
 
+# MAIN ROUTINE
 while True:
-    char = getch()
-    if len(directory()) != 0:
-        if char == '\x1b':
-            next_char = getch()
-            if next_char == '[':
-                next_char = getch()
-                if next_char == 'A':
-                    # up
-                    index = (index - 1) % len(directory())
-                elif next_char == 'B':
-                    # down
-                    index = (index + 1) % len(directory())
-                elif next_char == 'C':
-                    # right
-                    selection = os.path.abspath(os.getcwd()) + '/' + directory()[index]
-                    if os.path.isdir(selection):
-                        os.chdir(selection)
-                    elif os.path.isfile(selection):
-                        clear()
-                        print(selection)
-                        break
-                    if len(directory()) > 0:
-                        index = index % len(directory())
-                    else:
-                        index = 0
-                elif next_char == 'D':
+    index_dir() # update index
+    match getch():
+        # quit
+        case 'q':
+            break
+        # toggle hidden
+        case 'h':
+            temp_name = directory()[index]
+            hidden = not hidden
+            if temp_name in directory(): # update index
+                index = directory().index(temp_name)
+            else:
+                index = 0
+        # instructions
+        case 'i':
+            clear()
+            print(instructions)
+            getch()
+        case 'd':
+            dimension = not dimension
+        case '\x1b':
+            if getch() == '[':
+                match getch():
                     # left
-                    os.chdir('..')
-                    index = index % len(directory())
-        elif char == 'q':
-            # q
-            break
-    else:
-        if char == '\x1b':
-            next_char = getch()
-            if next_char == '[':
-                next_char = getch()
-                if next_char == 'D':
-                    # print("left")
-                    os.chdir('..')
-                    index = 0
-        elif char == 'q':
-            # print("q")
-            break
+                    case 'D':
+                        os.chdir('..')
+                    # up
+                    case 'A' if len(directory()) > 0:
+                        index = index - 1
+                    # down
+                    case 'B' if len(directory()) > 0:
+                        index = index + 1
+                    # right
+                    case 'C' if len(directory()) > 0:
+                        selection = os.path.abspath(os.getcwd()) + '/' + directory()[index]
+                        if os.path.isdir(selection):
+                            os.chdir(selection)
+                        elif os.path.isfile(selection):
+                            clear()
+                            print(selection)
+                            break
+                    case _:
+                        pass
+        case _:
+            pass
     dir_printer()
 
 os.chdir(local_folder)
